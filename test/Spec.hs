653876a39fd6d89af1fd0305a935e5c8ad416768
Donaim <3Donaim@gmail.com>
2019-07-23 21:05:00 +0000

    hard matching

--- test/Spec.hs
+++ test/Spec.hs
@@ -49,6 +49,9 @@ strs = map snd coms
 exprs :: [[String]]
 exprs = map (map fst) strs
 
+corrects :: [[String]]
+corrects = map (map snd) strs
+
 treeTokens :: [[[Expr]]]
 treeTokens = map (map tokenize) exprs
 
@@ -81,6 +84,38 @@ simplifiedStrings = map simpliString simplified
 
 simplifiedStringsLasts :: [[String]]
 simplifiedStringsLasts = map (map last) simplifiedStrings
+
+-----------
+-- MATCH --
+-----------
+
+notMatches :: [Maybe (String, String, Int, Int)]
+notMatches = maybify
+	where
+	m :: [[Maybe (String, String)]]
+	m = map equalMatchQ0 (zip simplifiedStringsLasts corrects)
+
+	r1 :: [[(Int, Maybe (String, String))]]
+	r1 = map (zip [0 ..]) m
+	r2 :: [(Int, [(Int, Maybe (String, String))])]
+	r2 = zip [0 ..] r1
+	inject :: [[(Int, Int, Maybe (String, String))]]
+	inject = map (\(x, arr) -> map (\(y, m) -> (x, y, m)) arr) r2
+	flat :: [(Int, Int, Maybe (String, String))]
+	flat = concat inject
+	maybify :: [Maybe (String, String, Int, Int)]
+	maybify = map maybeF flat
+
+	maybeF :: (Int, Int, Maybe (String, String)) -> Maybe (String, String, Int, Int)
+	maybeF (x, y, m) = case m of
+		Just (s1, s2) -> Just (s1, s2, x, y)
+		Nothing -> Nothing
+
+equalMatchQ0 :: ([String], [String]) -> [Maybe (String, String)]
+equalMatchQ0 (results, corrects) = map equalMatchQ1 (zip results corrects)
+
+equalMatchQ1 :: (String, String) -> Maybe (String, String)
+equalMatchQ1 (s1, s2) = if s1 == s2 then Nothing else Just (s1, s2)
 
 ----------
 -- MAIN --
