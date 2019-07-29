0ff3fdf58e239a271ea66d3d7cbce7d03fbd0f36
Donaim <3Donaim@gmail.com>
2019-07-29 23:09:09 +0200

    add reexport module

---
+++ src/PatternT/All.hs
@@ -0,0 +1,18 @@
+
+module PatternT.All
+	( module PatternT.Types
+	, module PatternT.Util
+	, module PatternT.Core
+	, module PatternT.SimplifyInterface
+	, module PatternT.Parsing
+	, module PatternT.Display
+	, module PatternT.MonadicRules
+	) where
+
+import PatternT.Core
+import PatternT.Display
+import PatternT.MonadicRules
+import PatternT.Parsing
+import PatternT.SimplifyInterface
+import PatternT.Types
+import PatternT.Util
