b6f6b7883074480d03c5f808c71a3de6181fddcc
Donaim <3Donaim@gmail.com>
2019-08-04 11:59:17 +0200

    remove useless utils

--- src/PatternT/Util.hs
+++ src/PatternT/Util.hs
@@ -2,7 +2,6 @@
 module PatternT.Util where
 
 import Text.Read (readMaybe)
-import Debug.Trace
 import PatternT.Types
 
 maybeDefault :: a -> (a -> Maybe a) -> a
@@ -18,18 +17,6 @@ swapEither :: Either a b -> Either b a
 swapEither e = case e of
 	Left a -> Right a
 	Right b -> Left b
-
--- | Strip all trailing zeroes
-showNoZeroes :: (Show a) => a -> String
-showNoZeroes x = if anydotq then striped else s
-	where
-		s = show x
-		r = reverse s
-		anydotq = any (== '.') s
-		striped = reverse $ (dropWhile (== '.') . dropWhile (== '0')) r
-
-traceS :: (Show a) => String -> a -> a
-traceS text x = trace (text ++ show x) x
 
 either3 :: (a -> d) -> (b -> d) -> (c -> d) -> Tuple3 a b c -> d
 either3 f g h me = case me of
